8093b20101189cc3cdb9231b20001118825675ee
dude0195 <78264858+dude0195@users.noreply.github.com>
2021-02-09 14:15:34 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,79 @@
+$(document).ready(function () {
+  const flowersSlider = new Swiper('.flowers-slider', {
+    // Параметры слайдера
+    loop: true,
+    slidesPerView: 6, 
+
+
+    // Подключаем стрелки
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+     breakpoints: {
+      // Если браузер >= 320px
+      320: {
+        slidesPerView: 2
+      },
+
+      // Если браузер >= 480px
+      480: {
+        slidesPerView: 3
+      },
+       
+      // Если браузер >= 992px
+      992: {
+        slidesPerView: 6
+      }
+    }
+
+
+  });
+
+  const reviewsSlider = new Swiper('.reviews-slider', {
+    // Параметры слайдера
+    loop: true,
+    slidesPerView: 1, 
+
+
+    // Подключаем стрелки
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  }); 
+
+  $("#review-1").on('click', function() {
+
+  $.fancybox.open([
+    {
+      src  : 'https://source.unsplash.com/0JYgd2QuMfw/1500x1000',
+      opts : {
+        caption : 'Second caption',
+        thumb   : 'https://source.unsplash.com/0JYgd2QuMfw/240x160'
+      }
+    },
+    {
+      src  : 'https://source.unsplash.com/IvfoDk30JnI/1500x1000',
+      opts : {
+        caption : 'First caption',
+        thumb   : 'https://source.unsplash.com/IvfoDk30JnI/240x160'
+      }
+    },
+    {
+      src  : 'https://source.unsplash.com/0JYgd2QuMfw/1500x1000',
+      opts : {
+        caption : 'Second caption',
+        thumb   : 'https://source.unsplash.com/0JYgd2QuMfw/240x160'
+      }
+    }
+  ], {
+    loop : true,
+    thumbs : {
+      autoStart : true
+    }
+  });
+
+});
+});
